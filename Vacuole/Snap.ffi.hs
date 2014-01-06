88e412d3f04dea01d81fcd40c3d23cc4de951e8d
Anton Nikishaev <me@lelf.lu>
2014-01-06 20:33:08 +0400

    Snap: no JS prefixes
    Ignore-this: 47cd1b2a167ef5e6d1365b34e651a11
    
    darcs-hash:20140106163308-055d6-148ba8fe4bde20261aaa0611746305aa0118b79e

--- Vacuole/Snap.ffi.hs
+++ Vacuole/Snap.ffi.hs
@@ -3,18 +3,20 @@ module Vacuole.Snap where
 import Haste
 import Haste.Prim
 
-data Paper
-data Element
+data Paper_
+data Element_
 
-type JSPaper = Ptr Paper
-type JSElement = Ptr Element
+type Paper = Ptr Paper_
+type Element = Ptr Element_
 
-instance Marshal (Ptr Paper)
+instance Marshal Paper
 
 
-foreign import cpattern "Snap(%1)" snap :: JSString -> IO (Ptr Paper)
-foreign import cpattern "%4.circle(%1,%2,%3)" circle :: Int -> Int -> Int -> JSPaper -> IO JSElement
-foreign import cpattern "%1.g()" g :: JSPaper -> IO JSElement
-foreign import cpattern "%2.append(%1)" elemAppend :: JSElement -> JSElement -> IO JSElement
-foreign import cpattern "%4.text(%1,%2,%3)" text :: Int -> Int -> JSString -> JSPaper -> IO JSElement
+foreign import cpattern "Snap(%1)" snap :: JSString -> IO Paper
+foreign import cpattern "%4.circle(%1,%2,%3)" circle_ :: Int -> Int -> Int -> Paper -> IO Element
+foreign import cpattern "%1.g()" g :: Paper -> IO Element
+foreign import cpattern "%2.append(%1)" elemAppend :: Element -> Element -> IO Element
+foreign import cpattern "%4.text(%1,%2,%3)" text :: Int -> Int -> JSString -> Paper -> IO Element
 
+circle :: (Int,Int) -> Int -> Paper -> IO Element
+circle (x,y) r = circle_ x y r
