b8308d5d62e1ddd277ea2ec18ad74fd9996ae3b2
Anton Nikishaev <me@lelf.lu>
2014-01-07 19:33:42 +0400

    Snap: path,marker
    Ignore-this: 682fc38fed556c502f520bb8ee682396
    
    darcs-hash:20140107153342-055d6-d0a327c75186074202650b4a0f14d658471e6c0f

--- Vacuole/Snap.ffi.hs
+++ Vacuole/Snap.ffi.hs
@@ -30,10 +30,12 @@ instance Marshal Attr where
 foreign import cpattern "Snap(%1)" snap :: JSString -> IO Paper
 foreign import cpattern "%4.circle(%1,%2,%3)" circle_ :: Int -> Int -> Int -> Paper -> IO Element
 foreign import cpattern "%5.line(%1,%2,%3,%4)" line_ :: Int->Int->Int->Int->Paper->IO Element
+foreign import cpattern "%2.path(%1)" path_ :: JSString->Paper->IO Element
 foreign import cpattern "%1.g()" g :: Paper -> IO Element
 foreign import cpattern "%2.append(%1)" append :: Element -> Element -> IO Element
 foreign import cpattern "%4.text(%1,%2,%3)" text_ :: Int -> Int -> JSString -> Paper -> IO Element
 foreign import cpattern "%1.attr(%2)" setAttrs_ :: Element -> JSAny -> IO Element
+foreign import cpattern "%7.marker(%1,%2,%3,%4,%5,%6)" marker_ :: Int->Int->Int->Int->Int->Int->Element->IO Element
 
 circle :: (Int,Int) -> Int -> Paper -> IO Element
 circle (x,y) r = circle_ x y r
@@ -43,6 +45,13 @@ text (x,y) t = text_ x y (toJSString t)
 
 line :: (Int,Int) -> (Int,Int) -> Paper -> IO Element
 line (x,y) (x',y') = line_ x y x' y'
+
+path :: String -> Paper -> IO Element
+path p = path_ (toJSString p)
+
+marker :: (Int,Int) -> (Int,Int) -> (Int,Int) -> Element -> IO Element
+marker (x,y) (w,h) (xref,yref) = marker_ x y w h xref yref
+
 
 foreign import cpattern "JSON.parse(%1)" jsParseJSON :: JSString -> JSAny
 
