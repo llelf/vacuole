c0ad7c8ae40c960d1d87187d5e38ce81a9651384
Anton Nikishaev <me@lelf.lu>
2014-01-11 10:19:37 +0400

    Types: Kind -> Purpose
    Ignore-this: f4cdf6e11cd532461f8f93d780cfd89a
    
    darcs-hash:20140111061937-055d6-6409de68b9cc4a2e0a78864bc195f5cda5e70073

--- Vacuole/View/Types.hs
+++ Vacuole/View/Types.hs
@@ -1,17 +1,28 @@
-module Vacuole.View.Types (Kind(..),
+module Vacuole.View.Types (Purpose(..),
                            Node(..),Link(..)) where
 
 
-data Kind = Vanilla | Cons | ArrWords | Nowhere
-          deriving (Eq,Show,Read)
+-- data Kind = Vanilla | Cons | ArrWords | Nowhere
+--           deriving (Eq,Show,Read)
 
 
+data Purpose
+    = Vanilla String
+    | EmptyList
+    | Cons
+    | ArrWords
+    | Fun
+    | Nowhere
+      deriving (Read,Show,Eq)
+
+data Node = Node Purpose String
+            deriving Show
 
 
-data Node = Node {
-      kind::Kind, name::String, desc::String
-} deriving Show
+-- data Node = Node {
+--       kind::Kind, name::String, desc::String
+-- } deriving Show
 
 
 data Link = Link { from, to :: Int }
-
+            deriving Show
