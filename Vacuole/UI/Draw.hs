288e05b3a8c7add392611345f5490b203e1d4854
Antonio Nikishaev <me@lelf.lu>
2014-02-25 15:12:11 +0400

    UI.Draw.Nodes
    Ignore-this: add36be1023cd0b6d8721d8a5a7cad0c
    
    darcs-hash:20140225111211-3ca48-f0d8af7e78754f955035e537a490a00347cd9c81

--- Vacuole/UI/Draw.hs
+++ Vacuole/UI/Draw.hs
@@ -15,6 +15,7 @@ import qualified Vacuole.UI.Vec as Vec
 import Vacuole.UI.Multi
 import Vacuole.UI.Draw.Links
 import Vacuole.UI.Draw.Paper
+import Vacuole.UI.Draw.Nodes
 import Vacuole.Snap
 
 foreign import ccall canvasClear :: IO ()
@@ -29,36 +30,6 @@ foreign import ccall drawGraph
 
 
 linkStrength _ = 0.6
-
-
-mkNode :: Node -> Paper -> IO Element
-mkNode (Node (Vanilla s) _) = vanillaNode s
-mkNode (Node EmptyList _)  = vanillaNode "[]"
-mkNode (Node ArrWords _) = memNode
-mkNode (Node ArrPtrs _) = memNode
-mkNode (Node Cons _) = consNode
-mkNode (Node Fun _)  = vanillaNode "λ"
-mkNode (Node Nowhere _) = nowhereNode
-
-
-nowhereNode = genericNode 0 "nowhere" "…"
-
-consNode = genericNode 15 "cons" "(:)"
-
-
-memNode p = do
-  cs <- forM [3,2..0] $ \x ->
-          circle (x*3,x*3) 20 p >>= setAttrs [(Class,"mem")]
-  g <- g p
-  foldM append g cs
-
-vanillaNode = genericNode 20 "vanilla"
-
-genericNode size svgcls str p = do
-  c <- circle (0,0) size p >>= setAttrs [(Class, svgcls)]
-  t <- text (0,0) str p >>= setAttrs [(TextAnchor,"middle"),
-                                      (AlignmentBaseline,"middle")]
-  g p >>= flip append c >>= flip append t
 
 
 draw :: [Element] -> Map.IntMap LinkElem -> IO ()
