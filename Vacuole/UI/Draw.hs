a6f26bae32989ed11e55120f5f2bd8ab2c920556
Antonio Nikishaev <me@lelf.lu>
2014-02-25 14:37:12 +0400

    svg circle classes
    Ignore-this: 9cd2bb5f12019d78dd82b4d20fa87f78
    
    darcs-hash:20140225103712-3ca48-cf154b3b16af801faa262794e502d7bc505a1389

--- Vacuole/UI/Draw.hs
+++ Vacuole/UI/Draw.hs
@@ -53,21 +53,21 @@ mkNode (Node Fun _)  = vanillaNode "λ"
 mkNode (Node Nowhere _) = nowhereNode
 
 
-nowhereNode = genericNode 3 "xx"
+nowhereNode = genericNode 0 "nowhere" "…"
 
-consNode = genericNode 15 "(:)"
+consNode = genericNode 15 "cons" "(:)"
 
 
 memNode p = do
   cs <- forM [3,2..0] $ \x ->
-          circle (x*3,x*3) 20 p >>= setAttrs [(Class,"c")]
+          circle (x*3,x*3) 20 p >>= setAttrs [(Class,"mem")]
   g <- g p
   foldM append g cs
 
-vanillaNode = genericNode 20
+vanillaNode = genericNode 20 "vanilla"
 
-genericNode size str p = do
-  c <- circle (0,0) size p >>= setAttrs [(Class,"c")]
+genericNode size svgcls str p = do
+  c <- circle (0,0) size p >>= setAttrs [(Class, svgcls)]
   t <- text (0,0) str p >>= setAttrs [(TextAnchor,"middle"),
                                       (AlignmentBaseline,"middle")]
   g p >>= flip append c >>= flip append t
