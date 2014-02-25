92e7efca5bd6e0b84c074150ac8a9cb3d426adcc
Antonio Nikishaev <me@lelf.lu>
2014-02-25 13:57:22 +0400

    misc.js
    Ignore-this: 81918ef6bb4cd581d0f57f1211ab9e31
    
    darcs-hash:20140225095722-3ca48-1be16d738fdaabc9e76fcab10b3731b0a5897c17

---
+++ uibits/misc.js
@@ -0,0 +1,11 @@
+
+$('.wut .show').click (function () { $('.wut .desc').toggle() });
+
+$('.wut .examples').each (function (i,e)
+			  {
+			    $(e).click(function (r,o,q) {
+			      console.log($(this).text()) });
+			  });
+  
+
+
