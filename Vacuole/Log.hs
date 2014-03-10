d4a0d00954b0bc9071796f1958d2e536a686fa38
Antonio Nikishaev <antonio@lelf.me>
2014-03-10 18:00:54 +0400

    Vacuole.Log
    Ignore-this: 6975e75ac5c337398edd34c05b9320a4
    
    darcs-hash:20140310140054-898ea-3367af56b6cbf557462162a9b93119318a8d0a41

---
+++ Vacuole/Log.hs
@@ -0,0 +1,14 @@
+module Vacuole.Log (logExpr) where
+
+import Database.SQLite.Simple
+import Database.SQLite.Simple.FromField
+import Data.Time.Clock
+
+logExpr :: String -> String -> String -> IO ()
+logExpr ip expr status =
+    withConnection "log.db" $ \conn -> do
+      time <- getCurrentTime
+      execute conn "insert into log (time,ip,expr,status) values (?,?,?,?)"
+                  (time,ip,expr,status)
+
+
