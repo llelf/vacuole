0ecabcb40fee826a1e36509c7c53b91c3cbc56ca
Antonio Nikishaev <a@lelf.me>
2014-05-05 14:25:16 +0400

    main: use Log.log
    Ignore-this: 86bd8911821dc13f486d7a8e9dfae5da
    
    darcs-hash:20140505102516-7e7a4-8c73107abddd1176e6ec8379c768271ff15458ea

--- Vacuole/Serve.hs
+++ Vacuole/Serve.hs
@@ -3,24 +3,25 @@
 
 import Web.Scotty
 import Network.Wai.Middleware.Static
+--import Network.Wai.Middleware.RequestLogger
 import Data.Text.Lazy.Encoding
 import Data.Text.Lazy (pack,unpack)
 import Data.Monoid
 import Control.Monad.IO.Class
 
+import Vacuole.Log as Log
 import Vacuole.View
 
 
 main = scotty 5555 $ do
          middleware static
+--         middleware logStdoutDev
          post "/vac" $ do
                         e <- body
-                        b <- liftIO . boo . unpack . decodeUtf8 $ e
+                        let expr = unpack . decodeUtf8 $ e
+                        b <- liftIO $ boo expr
                         liftIO $ print (e,b)
+                        liftIO $ Log.logExpr "" expr ""
                         text . pack $ show b
-
-
-
-
 
 
